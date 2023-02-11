b538a8f9f28a9bef9af26c9c0fe6d7a59a0fa76c
IkanAsinn <qbin.hermawan@gmail.com>
2023-02-11 18:12:16 +0700

    add pagination in finduser view

--- BootCoin/Controllers/FindUserController.cs
+++ BootCoin/Controllers/FindUserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using BootCoin.Models;
 using BootCoin.Models.DBEntities;
+using System.Dynamic;
 
 namespace BootCoin.Controllers
 {
@@ -9,48 +10,82 @@ namespace BootCoin.Controllers
     {
         public readonly ApplicationDbContext _context;
         public List<FindUserModel> participantsList = new List<FindUserModel>();
-
+        public List<FindUserModel> result = new List<FindUserModel>();
 
         public FindUserController(ApplicationDbContext context)
         {
             _context = context;
+            var participants = _context.Participants.ToList();
+            foreach (var participant in participants)
+            {
+                FindUserModel p = new FindUserModel();
+                p.ParticipantName = participant.ParticipantName;
+                p.Group = _context.Group.Where(g => g.GroupID == participant.GroupID).Select(g => g.GroupName).FirstOrDefault();
+                p.TotalCoins = participant.TotalCoins;
+                participantsList.Add(p);
+            }
 
-            participantsList = _context.Participants
-                .Join(_context.Group,
-                p => p.GroupID,
-                g => g.GroupID,
-                (p, g) => new { p, g })
-                .Select(pg => new FindUserModel()
-                {
-                    ParticipantName = pg.p.ParticipantName,
-                    Group = pg.g.GroupName,
-                    TotalCoins = pg.p.TotalCoins
-                }).ToList();
         }
-
 
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
-        public IActionResult Search(string ToFind)
+        [HttpPost]
+        public IActionResult Search(string ToFind, int page = 1)
         {
+            if (page < 1)
+            {
+                page = 1;
+            }
+            List<FindUserModel> users = new List<FindUserModel>();
+            result.Clear();
+
             if (!string.IsNullOrEmpty(ToFind))
             {
-                List<FindUserModel> users = new List<FindUserModel>();
-                foreach (var participant in participantsList)
-                {
-                    if (participant.ParticipantName.ToUpper().Contains(ToFind.ToUpper()))
-                    {
-                        users.Add(participant);
-                    }
-                }
-                return PartialView("_Search", users);
+                users = getUsers(ToFind);
             }
 
-            return PartialView("_Search", participantsList);
+
+            int pCount = (users.Count() > 0) ? users.Count() : participantsList.Count();
+            int pageSize = (users.Count() > 0 && users.Count() < 5) ? users.Count() : 5;
+            Pager pager = new Pager(pCount, page, pageSize);
+            int pSkip = (page - 1) * pageSize;
+
+            if (users.Count() > 0)
+            {
+                for (int i = pSkip; i < pSkip + pageSize; i++)
+                {
+                    result.Add(users[i]);
+                }
+            }
+            else
+            {
+                for (int i = pSkip; i < pSkip + pageSize; i++)
+                {
+                    result.Add(participantsList[i]);
+                }
+            }
+
+            dynamic mymodel = new ExpandoObject();
+            mymodel.pager = pager;
+            mymodel.participantsList = result;
+
+            return PartialView("_Search", mymodel);
+        }
+
+        public List<FindUserModel> getUsers(string ToFind)
+        {
+            List<FindUserModel> users = new List<FindUserModel>();
+            foreach (var participant in participantsList)
+            {
+                if (participant.ParticipantName.ToUpper().Contains(ToFind.ToUpper()))
+                {
+                    users.Add(participant);
+                }
+            }
+            return users;
         }
     }
 }
---
+++ BootCoin/Models/Pager.cs
@@ -0,0 +1,50 @@
+﻿
+namespace BootCoin.Models
+{
+    public class Pager
+    {
+
+        public int TotalItems { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+        public Pager(){
+
+        }
+
+        public Pager(int totalItems, int pageNumber, int pageSize = 10)
+        {
+            int totalPages = totalItems/pageSize;
+            int currentPage = pageNumber;
+
+            int startPage = currentPage-5;
+            int endPage = currentPage+4;
+
+            if(startPage < 1)
+            {
+                endPage = endPage - (startPage - 1);
+                startPage = 1;
+            }
+
+            if(endPage > totalPages)
+            {
+                endPage = totalPages;
+                if(endPage > 10)
+                {
+                    startPage = endPage - 9;
+                }
+            }
+
+            TotalItems = totalItems;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPages = totalPages;
+            StartPage = startPage;
+            EndPage = endPage;
+        }
+
+    }
+}
